4a76f751e9d67ff650e3fcb1bfa2fbc410200ded
imranhosain6554 <imran122.office@gmail.com>
2023-06-07 14:43:47 +0600

    fix stage2

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 import cors from 'cors'
-import express, { Application, Request } from 'express'
+import express, { Application } from 'express'
 const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (req: Request, res: Response) => {
+app.get('/', (reqss, res) => {
   res.send('server running successfully!')
 })
 
