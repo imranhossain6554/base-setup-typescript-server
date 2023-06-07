5c11c0149745243f4e916f5aabae01cbd565d9ec
imranhosain6554 <imran122.office@gmail.com>
2023-06-07 14:55:50 +0600

    fix all3

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const app: Application = express()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.get('/', (reqsds, res) => {
+get('/', (reqss, res) => {
   res.send('server running successfully!')
 })
 
