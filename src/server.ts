4784ba72255b040afe1d0708082548df524d34a9
imranhosain6554 <imran122.office@gmail.com>
2023-06-08 13:04:45 +0600

    fix exlint2

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
 import mongoose from 'mongoose'
-import config from './config/index'
 import app from './app'
+import config from './config/index'
 
 async function boostrap() {
   try {
     await mongoose.connect(config.database_url as string)
+    // eslint-disable-next-line no-console
     console.log(`🛢   Database is connected successfully`)
 
     app.listen(config.port, () => {
+      // eslint-disable-next-line no-console
       console.log(`Application  listening on port ${config.port}`)
     })
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log('Failed to connect database', err)
   }
 }
